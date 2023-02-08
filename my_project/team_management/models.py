e419eefb6d20bb072eb5be9227b5a2a88ecd6218
Xinyu Kang <kangxinyu0423@126.com>
2023-02-08 14:38:54 -0500

    change role to dropdrown

--- my_project/team_management/models.py
+++ my_project/team_management/models.py
@@ -3,12 +3,19 @@ from phone_field import PhoneField
 
 # Create your models here.
 
+role_choices = (
+        ("REGULAR", "Regular - Can't delete members"),
+        ("ADMIN", "Admin - Can delete members")
+    )
+    
 class Employee(models.Model):
     # id, first name, last name, email, phone number, role: regular vs admin
     id = models.AutoField(primary_key=True) # make the id unique
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
-    role = models.CharField(max_length=300)  # TODO change this to a selection
+
+    role = models.CharField(max_length=200, choices=role_choices, default="REGULAR")
+
     salary = models.IntegerField(null=True)
     phone_number = PhoneField(blank=True, help_text='Contact phone number')
